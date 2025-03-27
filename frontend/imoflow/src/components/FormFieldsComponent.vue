d772be7945ccae297263f0be4163c053565a4c6c
Elias Mateus <eliasmateusmgoncalves@gmail.com>
2025-03-27 01:02:25 -0300

    style: enhance FormFieldsComponent layout with responsive design and improved field organization

--- frontend/imoflow/src/components/FormFieldsComponent.vue
+++ frontend/imoflow/src/components/FormFieldsComponent.vue
@@ -51,63 +51,83 @@ const condominiumTypes = computed(() => Object.values(CondominiumType))
 
 <template>
   <fieldset :disabled="isDisabled" class="w-100">
-    <div
-      v-for="(label, key) in {
-        nome_condominio: 'Nome do Condomínio',
-        cep: 'CEP',
-        rua: 'Rua',
-        numero: 'Número',
-        complemento: 'Complemento',
-        cidade: 'Cidade',
-      }"
-      :key="key"
-      class="mb-3"
-    >
-      <label class="form-label" :for="key">{{ label }}</label>
-      <input
-        :id="key"
-        :type="key === 'cep' || key === 'numero' ? 'number' : 'text'"
-        class="form-control"
-        v-model="form[key]"
-        :required="key !== 'complemento'"
-      />
-    </div>
-
-    <div class="mb-3">
-      <label class="form-label" for="estado">Estado</label>
-      <select id="estado" class="form-select" v-model="form.estado" required>
-        <option value="" disabled>Selecione</option>
-        <option v-for="estado in estados" :key="estado" :value="estado">
-          {{ estado }}
-        </option>
-      </select>
-    </div>
-
-    <div class="mb-3">
-      <label class="form-label">Tipo do Condomínio</label>
-      <div class="form-check">
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label class="form-label" for="nome_condominio">Nome do Condomínio</label>
         <input
-          class="form-check-input"
-          type="radio"
-          id="tipo-horizontal"
-          v-model="form.tipo"
-          :value="CondominiumType.HORIZONTAL"
+          id="nome_condominio"
+          type="text"
+          class="form-control"
+          v-model="form.nome_condominio"
+          required
         />
-        <label class="form-check-label" for="tipo-horizontal">
-          {{ CondominiumType.HORIZONTAL }}
-        </label>
       </div>
-      <div class="form-check">
-        <input
-          class="form-check-input"
-          type="radio"
-          id="tipo-vertical"
-          v-model="form.tipo"
-          :value="CondominiumType.VERTICAL"
-        />
-        <label class="form-check-label" for="tipo-vertical">
-          {{ CondominiumType.VERTICAL }}
-        </label>
+
+      <div class="col-md-6">
+        <label class="form-label" for="cep">CEP</label>
+        <input id="cep" type="number" class="form-control" v-model="form.cep" required />
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label class="form-label" for="rua">Rua</label>
+        <input id="rua" type="text" class="form-control" v-model="form.rua" required />
+      </div>
+
+      <div class="col-md-6">
+        <label class="form-label" for="numero">Número</label>
+        <input id="numero" type="number" class="form-control" v-model="form.numero" required />
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label class="form-label" for="complemento">Complemento</label>
+        <input id="complemento" type="text" class="form-control" v-model="form.complemento" />
+      </div>
+
+      <div class="col-md-6">
+        <label class="form-label" for="cidade">Cidade</label>
+        <input id="cidade" type="text" class="form-control" v-model="form.cidade" required />
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <label class="form-label" for="estado">Estado</label>
+        <select id="estado" class="form-select" v-model="form.estado" required>
+          <option value="" disabled>Selecione</option>
+          <option v-for="estado in estados" :key="estado" :value="estado">
+            {{ estado }}
+          </option>
+        </select>
+      </div>
+
+      <div class="col-md-6">
+        <label class="form-label">Tipo do Condomínio</label>
+        <div class="d-flex">
+          <div class="form-check me-4">
+            <input
+              class="form-check-input"
+              type="radio"
+              id="tipo-horizontal"
+              v-model="form.tipo"
+              :value="CondominiumType.HORIZONTAL"
+            />
+            <label class="form-check-label" for="tipo-horizontal"> Horizontals </label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="radio"
+              id="tipo-vertical"
+              v-model="form.tipo"
+              :value="CondominiumType.VERTICAL"
+            />
+            <label class="form-check-label" for="tipo-vertical"> Vertical </label>
+          </div>
+        </div>
       </div>
     </div>
   </fieldset>
